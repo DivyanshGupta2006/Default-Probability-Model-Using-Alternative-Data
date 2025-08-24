e5a3f224d7113d527f6bcf0d204d7e542dd5ce3d
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-08-24 18:34:10 +0530

    Fix bugs

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,8 +1,8 @@
 import os
 
 # path related variables
-RAW_DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'raw_data'))
-PROCESSED_DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'processed_data'))
+RAW_DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'raw_data'))
+PROCESSED_DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'processed_data'))
 
 # data related variables
 ID = "SK_ID_CURR"
