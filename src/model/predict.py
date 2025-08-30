6d4f7368c838c8ec334d07d547459d198095f0b6
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2025-08-31 02:53:12 +0530

    Add predict function

--- src/model/predict.py
+++ src/model/predict.py
@@ -1,2 +1,10 @@
-def make_prediction():
-    return 0
+from src.data_processing import preprocess
+from src.utils import read_file, get_config
+
+config = get_config.read_yaml_from_package()
+
+def make_prediction(input_df):
+    input_df = preprocess.clean(input_df, use_saved=True)
+    model = read_file.read_model_data(config['model'])
+    preds = model.predict_proba(input_df)
+    return preds
